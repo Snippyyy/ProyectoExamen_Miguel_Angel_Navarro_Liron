76f05052b09965d0e7f8b111d326e0278376a00c
Snippyyy <Snippy_06@hotmail.com>
2024-11-13 17:02:31 +0100

    Ejericicio 6
    He creado la migración donde añado la FK a la tabla users y he definido las relaciones BelongsTo y HasMany correspondientemente (1:n)

--- app/Models/Roles.php
+++ app/Models/Roles.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Roles extends Model
 {
@@ -15,4 +16,8 @@ class Roles extends Model
         'display_name',
         'description'
     ];
+
+    public function users(): HasMany{
+        return $this->hasMany(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -51,5 +53,8 @@ class User extends Authenticatable implements MustVerifyEmail
     public function posts(): HasMany
     {
         return $this->hasMany(Post::class);
+    }
+    public function roles(): BelongsTo{
+        return $this->belongsTo(Roles::class);
     }
 }
